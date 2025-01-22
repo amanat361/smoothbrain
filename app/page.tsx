9522caaf226e83ae66caeb30b26f9856240b4d2c
vlmontegrande <vlmontegrande@gmail.com>
2025-01-22 11:57:17 -0800

    Create custom button components for join/create lobby page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Button from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
-
+      <Button>Create Lobby</Button>
+      <Button>Join Lobby</Button>
     </main>
   );
 }
