b88dc242e6d54e6a4cea7e11b80e9c59dcc9e2fb
vlmontegrande <vlmontegrande@gmail.com>
2025-01-24 00:04:07 -0800

    Retool home page components with catalyst components

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
 import { Button } from "@/components/catalyst/button";
+import { Heading } from "@/components/catalyst/heading";
 
 export default function Home() {
   return (
-    <main>
-      <header>
-        <h1 className="">Smoothbrain</h1>
+    <main className="h-screen">
+      <header className="h-1/5 flex justify-center items-center">
+        <Heading className="">Smoothbrain</Heading>
       </header>
-      <nav>
-        <ul>
-          <li>
-            <Button className="block" onClick={() => alert("Created a lobby!")}>Create Lobby</Button>
-          </li>
-          <li>
-            <Button className="block" onClick={() => alert("Joined a lobby!")}>Join Lobby</Button>
-          </li>
-        </ul>
+      <nav className="h-4/5 flex flex-col items-center justify-center">
+        <Button className="">Create Lobby!</Button>
+        <Button className="">Join Lobby!</Button>
       </nav>
     </main>
   );
